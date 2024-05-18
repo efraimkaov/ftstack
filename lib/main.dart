5a895ce103c6e7a3d30f728b65f71a130da27b19
efraimkaov <efkaov@gmail.com>
2024-05-18 16:02:41 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,44 +19,83 @@ class LayoutBuilderExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          if (constraints.maxWidth > constraints.maxHeight) {
-            return _buildWideContainers();
-          } else {
-            return _buildNormalContainer();
-          }
-        },
-      ),
-    );
-  }
-
-  Widget _buildNormalContainer() {
-    return Center(
-      child: Container(
-        height: 100.0,
-        width: 100.0,
-        color: Colors.red,
-      ),
-    );
-  }
-
-  Widget _buildWideContainers() {
-    return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.red,
-          ),
-          Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.yellow,
-          ),
-        ],
+      backgroundColor: Colors.brown,
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            double whiteHeight = constraints.maxHeight / 18;
+            double blackHeight = whiteHeight / 1.5;
+            if (constraints.maxWidth > constraints.maxHeight) {
+              return Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellow,
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return Stack(
+                children: [
+                  Positioned(
+                    top: whiteHeight * 0,
+                    left: constraints.maxWidth / 7,
+                    width: constraints.maxWidth / 7 * 6,
+                    height: whiteHeight,
+                    child: Container(
+                      color: Colors.green,
+                    ),
+                  ),
+                  Positioned(
+                    top: whiteHeight * 1,
+                    left: constraints.maxWidth / 7,
+                    width: constraints.maxWidth / 7 * 6,
+                    height: whiteHeight,
+                    child: Container(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Positioned(
+                    top: whiteHeight * 2,
+                    left: constraints.maxWidth / 7,
+                    width: constraints.maxWidth / 7 * 6,
+                    height: whiteHeight,
+                    child: Container(
+                      color: Colors.green,
+                    ),
+                  ),
+                  Positioned(
+                    top: whiteHeight * 1 - blackHeight / 2,
+                    left: constraints.maxWidth / 7,
+                    width: constraints.maxWidth / 7 * 3,
+                    height: blackHeight,
+                    child: Container(
+                      color: Colors.purple,
+                    ),
+                  ),
+                  Positioned(
+                    top: whiteHeight * 2 - blackHeight / 2,
+                    left: constraints.maxWidth / 7,
+                    width: constraints.maxWidth / 7 * 3,
+                    height: blackHeight,
+                    child: Container(
+                      color: Colors.purple,
+                    ),
+                  ),
+                ],
+              );
+            }
+          },
+        ),
       ),
     );
   }
